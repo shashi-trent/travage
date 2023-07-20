b2d741ccf89f355b54d614cd339ff2214bd21f3a
shashi-trent <shashitre@gmail.com>
2023-07-20 18:25:32 +0530

    spring boot setup with a volatile in-memory h2db (sql db)

---
+++ src/main/java/test/nymble/travage/TravageApplication.java
@@ -0,0 +1,13 @@
+package test.nymble.travage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/nymble/travage/TravageApplicationTests.java
@@ -0,0 +1,13 @@
+package test.nymble.travage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
